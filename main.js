6d51c61dfde340f030d68396f24473c97c087630
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-10-24 11:55:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function preload() {
+lipX=0;
+lipY=0;
 
+function preload() {
+lipstick = loadImage('https://i.postimg.cc/PrN9LWrR/Lips.png');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
-    canvas.center();
+    canvas.position(530, 200);
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
@@ -22,13 +25,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        lipX = results[0].pose.nose.x - 15;
+        lipY = results[0].pose.nose.y + 10;
+        console.log("lip x = " + lipX);
+        console.log("lip y = " + lipY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(lipstick, lipX, lipY, 40, 30);
 }
 
 function take_snapshot() {
